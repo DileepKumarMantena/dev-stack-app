2776957a4c0a7b2ae112fc2c0d5c467308bfde39
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-11-13 21:01:06 +0530

    added a code in set1

---
+++ Backend/Set1/python1.py
@@ -0,0 +1,7 @@
+'''
+Write a program that takes a string input from the user and displays it along with its data type
+'''
+
+
+user_input = input("Enter a string: ")
+print(f"The Data type is {type(user_input)} and value is '{user_input}'")
