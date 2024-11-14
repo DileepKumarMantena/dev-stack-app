f81c2812b13c1c707118995eec85e0f18a6407d4
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-11-14 19:36:35 +0530

    added second code in set1

---
+++ Backend/Set1/python2.py
@@ -0,0 +1,7 @@
+'''
+Write a program that converts a temperature from Celsius to Fahrenheit.
+'''
+
+celsius=int(input("Enter temparature for farenheit:"))
+farenheit=celsius*1.8+32
+print(f"THE Fahrenheit TEMPARATURE IS:{farenheit}")
